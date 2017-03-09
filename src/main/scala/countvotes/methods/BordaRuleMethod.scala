a30fd78412f204b66788086f211237536c30ca37
Deepesh Pandey <deepeshpandey17@gmail.com>
2017-03-09 16:21:31 +0100

    fixed the code as per the review comments

--- src/main/scala/countvotes/methods/BordaRuleMethod.scala
+++ src/main/scala/countvotes/methods/BordaRuleMethod.scala
@@ -1,14 +1,16 @@
 package countvotes.methods
 
 import countvotes.structures.{Candidate, Input, Rational, Report, _}
-
-import scala.collection.mutable
-import scala.collection.mutable.HashMap
+import countvotes.structures.{Candidate, Rational, _}
+import collection.mutable.{HashMap => Map}
 
 /**
   * Created by deepeshpandey on 07/03/17.
   */
-object BordaRuleMethod extends BordaRule[WeightedBallot]{
+object BordaRuleMethod extends VoteCountingMethod[WeightedBallot]{
+
+  private val result: Result = new Result
+  private val report: Report[WeightedBallot] = new Report[WeightedBallot]
 
   def runScrutiny(election: Election[WeightedBallot], candidates: List[Candidate], numVacancies: Int):   Report[WeightedBallot]  = {
 
@@ -25,6 +27,23 @@ object BordaRuleMethod extends BordaRule[WeightedBallot]{
     report.setWinners(winners(election, candidates, numVacancies))
 
     report
+  }
+
+  override def totals(election: Election[WeightedBallot], candidates: List[Candidate]): Map[Candidate, Rational] = {
+    val m = new Map[Candidate, Rational]
+    val totalCandidates = getCandidates(election).length
+
+    for (b <- election if !b.preferences.isEmpty) {
+      // need to take the size of the list first and then calculate the borda scores
+      var bordaCounter = totalCandidates
+
+      b.preferences.map(x => {
+        m(x) = m.getOrElse(x, new Rational(0,1)) + (bordaCounter-1)
+        bordaCounter -= 1
+      })
+
+    }
+    m
   }
 
   def winners(election: Election[WeightedBallot], ccandidates: List[Candidate], numVacancies: Int ):
