edc3d78112708e00ed258b68e66a492e14fa88c4
Deepesh Pandey <deepeshpandey17@gmail.com>
2017-03-17 12:22:10 +0100

    Improved the Coomb testing procedure

--- src/test/scala/countvotes/CoombTest.scala
+++ src/test/scala/countvotes/CoombTest.scala
@@ -12,38 +12,20 @@ import scala.collection.mutable.ListBuffer
   */
 class CoombTest extends Specification{
 
+  val expectedCoombWinnerList = List(Candidate("Nashville"))
 
   "Coomb Test" should {
-    "verify result" in { coombMethodVerification("18-example.txt", "18-candidates.txt") shouldEqual true }
-    //"verify result" in { coombMethodVerification("19-example.txt", "19-candidates.txt") shouldEqual true }
-
-
+    "verify result" in { coombMethodVerification("18-example.txt", "18-candidates.txt") shouldEqual expectedCoombWinnerList }
   }
 
-  def coombMethodVerification(electionFile: String, candidatesFile: String): Boolean = {
+  def coombMethodVerification(electionFile: String, candidatesFile: String): List[Candidate] = {
 
-    val candidates = CandidatesParser.read("/Users/deepeshpandey/Desktop/aossie/agora/Agora/files/Examples/" + candidatesFile)
-    val election =  PreferencesParser.read("/Users/deepeshpandey/Desktop/aossie/agora/Agora/files/Examples/" + electionFile)
-    val methodWinnersList = CoombRuleMethod.winners(election, candidates, 1)
-    val correctWinnerResult = getCoombWinnerList(candidates)
+    val candidates = CandidatesParser.read("../Agora/files/Examples/" + candidatesFile)
+    val election =  PreferencesParser.read("../Agora/files/Examples/" + electionFile)
 
-    var matchSize = methodWinnersList.zip(correctWinnerResult).filter(x => {x._1._1 == x._2}).size
-
-    matchSize == correctWinnerResult.length
+    CoombRuleMethod.winners(election, candidates, 1).map(pair => pair match {
+      case (candidate, rational) => candidate
+    })
 
   }
-
-  def getCoombWinnerList(candidates: List[Candidate]): List[(Candidate)] = {
-    // correct rankings from https://en.wikipedia.org/wiki/Schulze_method
-    val coombsWinnerList = new ListBuffer[Candidate]
-
-    coombsWinnerList.insert(0, new Candidate("Nashville"))
-
-    coombsWinnerList.toList
-  }
-
-
-
-
-
 }
