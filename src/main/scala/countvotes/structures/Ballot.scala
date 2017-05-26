2ca8c2bd9e47dd98f09210ae1defb374c37a97b6
SaumoPal97 <saumopal1997@gmail.com>
2017-05-26 09:01:16 +0530

    Added ScoredBallot

--- src/main/scala/countvotes/structures/Ballot.scala
+++ src/main/scala/countvotes/structures/Ballot.scala
@@ -3,6 +3,7 @@ package countvotes.structures
 import scala.languageFeature.implicitConversions
 
 class Ballot(val preferences: List[Candidate], val id: Int)
+class ScoredBallot(val preferences: List[(Candidate,Int)], val id: Int)
 
 trait Weight extends Ballot {
   val weight: Rational
@@ -18,7 +19,7 @@ trait Marking extends Ballot {
 
 
 class WeightedBallot(p: List[Candidate], id: Int, w: Rational)
-extends Ballot(p, id) with Weight {
+  extends Ballot(p, id) with Weight {
   val weight = w
   override def toString: String = "[" + id + ", " + p + ", " + w + "]"
 }
@@ -26,19 +27,39 @@ object WeightedBallot {
   def apply(p: List[Candidate], id: Int, w: Rational): WeightedBallot  = new WeightedBallot(p, id, w)
   //def apply(p: List[Candidate], id: Int, w: Rational = new Rational(1,1)) = new SimpleBallot(p, id, w)
   //def apply(p: List[Candidate], id: Int) = new SimpleBallot(p, id, new Rational(1,1))
- // implicit def fromBallot(b: Ballot) = {
- //   new WeightedBallot(b.preferences, b.id, 1) // note that the weight is assigned 1 here
- // }
+  // implicit def fromBallot(b: Ballot) = {
+  //   new WeightedBallot(b.preferences, b.id, 1) // note that the weight is assigned 1 here
+  // }
 }
 
 
 class MarkedWeightedBallot(p: List[Candidate], id: Int,  m: Boolean, w: Rational)
-extends WeightedBallot(p, id, w) with Marking {
+  extends WeightedBallot(p, id, w) with Marking {
   val marking = m
 }
 object MarkedWeightedBallot{
   def apply(p: List[Candidate], id: Int, m:Boolean, w: Rational): MarkedWeightedBallot = new MarkedWeightedBallot(p, id, m, w)
- // implicit def fromWeightedBallot(b: WeightedBallot) = {
- //   new MarkedWeightedBallot(b.preferences, b.id, true, b.weight) // note that the marking is assigned true here
- // }
+  // implicit def fromWeightedBallot(b: WeightedBallot) = {
+  //   new MarkedWeightedBallot(b.preferences, b.id, true, b.weight) // note that the marking is assigned true here
+  // }
 }
+
+class WeightedScoredBallot(p: List[(Candidate,Int)], id: Int, w: Rational)
+  extends ScoredBallot(p, id) with Weight {
+  val weight = w
+  override def toString: String = "[" + id + ", " + p + ", " + w + "]"
+}
+
+object WeightedScoredBallot {
+  def apply(p: List[(Candidate,Int)], id: Int, w: Rational): WeightedScoredBallot  = new WeightedScoredBallot(p, id, w)
+}
+
+
+class MarkedWeightedScoredBallot(p: List[(Candidate,Int)], id: Int,  m: Boolean, w: Rational)
+  extends WeightedScoredBallot(p, id, w) with Marking {
+  val marking = m
+}
+object MarkedWeightedScoredBallot{
+  def apply(p: List[(Candidate,Int)], id: Int, m:Boolean, w: Rational): MarkedWeightedScoredBallot = new MarkedWeightedScoredBallot(p, id, m, w)
+}
+
