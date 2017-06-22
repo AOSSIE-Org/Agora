ae6cc139e6133d8c23705cbbfa40d1585c79a1e6
Bruno Woltzenlogel Paleo <bruno.wp@gmail.com>
2017-06-22 16:10:31 +1000

    Refactor parsers: eliminate a trait that is used by a single class (such traits are pointless)

--- src/main/scala/countvotes/parsers/PreferencesParser.scala
+++ src/main/scala/countvotes/parsers/PreferencesParser.scala
@@ -18,19 +18,6 @@ trait ElectionParsers extends RegexParsers {
   def id: Parser[Int] = """[0-9]+""".r ^^ { _.toInt }
 }
 
-trait ChoiceParsers extends RegexParsers with ElectionParsers {
-  def choice: Parser[(Candidate, Option[Int], Option[Int])] = candidate ~ ";" ~ opt(rank) ~ ";" ~ opt(score) ^^ {
-        case ~(~(~(~(candidate, ";"), rank), ";"), score) => {
-          (candidate, rank, score)
-        }
-      }
-  
-  def preferences: Parser[List[(Candidate, Option[Int], Option[Int])]] = repsep(choice, ")(")
-  
-  def rank: Parser[Int] = """[0-9]+""".r ^^ { _.toInt }
-
-  def score: Parser[Int] = """[0-9]+""".r ^^ { _.toInt }
-}
 
 object PreferencesParser extends ElectionParser[WeightedBallot] with RegexParsers with ElectionParsers {
   
@@ -45,8 +32,20 @@ object PreferencesParser extends ElectionParser[WeightedBallot] with RegexParser
   }
 }
 
-object PreferencesParserWithRankAndScore extends ElectionParser[WeightedBallot] with RegexParsers with ElectionParsers with ChoiceParsers {
+object PreferencesParserWithRankAndScore extends ElectionParser[WeightedBallot] with RegexParsers with ElectionParsers {
 
+  def choice: Parser[(Candidate, Option[Int], Option[Int])] = candidate ~ ";" ~ opt(rank) ~ ";" ~ opt(score) ^^ {
+        case ~(~(~(~(candidate, ";"), rank), ";"), score) => {
+          (candidate, rank, score)
+        }
+      }
+  
+  def preferences: Parser[List[(Candidate, Option[Int], Option[Int])]] = repsep(choice, ")(")
+  
+  def rank: Parser[Int] = """[0-9]+""".r ^^ { _.toInt }
+
+  def score: Parser[Int] = """[0-9]+""".r ^^ { _.toInt }
+  
   // the method line returns a Parser of type ACTBallotPapersDataStructure
   def line: Parser[WeightedBallot] = id ~ numerator ~ "/" ~ denominator ~ "(" ~ preferences ~ ")" ^^ {
     case ~(~(~(~(~(~(i, n), "/"), d), "("), p), ")") => {
@@ -62,73 +61,6 @@ object PreferencesParserWithRankAndScore extends ElectionParser[WeightedBallot] 
     }
   }
 }
-
-//object PreferencesParserWithRankWithoutScore extends ElectionParser[WeightedBallot] with RegexParsers with ElectionParsers with ChoiceParsers {
-//
-//  // the method line returns a Parser of type ACTBallotPapersDataStructure
-//  def line: Parser[WeightedBallot] = id ~ numerator ~ "/" ~ denominator ~ "(" ~ preferences ~ ")" ^^ {
-//    case ~(~(~(~(~(~(i, n), "/"), d), "("), p), ")") => {
-//      //println(p)
-//      def line2: Parser[List[(Candidate, Int, Int)]] = candidate ~ ";" ~ rank ~ ";"  ^^ {
-//        case ~(~(~(candidate, ";"), rank), ";") => {
-//          List((candidate, rank, 0))
-//        }
-//      }
-//
-//      var temp: List[(Candidate, Int, Int)] = Nil
-//
-//      for (i <- p if !p.isEmpty) {
-//        parse(line2, i) match {
-//          case Success(sucLine, _) => temp = sucLine ::: temp
-//          case _ => throw new Exception("Should never happen")
-//        }
-//      }
-//
-//      temp = temp.sortWith(_._2 < _._2)
-//
-//      var ccand: List[Candidate] = Nil
-//
-//      for (f <- temp if !temp.isEmpty) {
-//        ccand = f._1 :: ccand
-//      }
-//
-//      WeightedBallot(ccand.reverse, i, Rational(n, d))
-//    }
-//  }
-//}
-//
-//object PreferencesParserWithoutRankWithScore extends ElectionParser[WeightedBallot] with RegexParsers with ElectionParsers with ChoiceParsers {
-//
-//  def line: Parser[WeightedBallot] = id ~ numerator ~ "/" ~ denominator ~ "(" ~ preferences ~ ")" ^^ {
-//    case ~(~(~(~(~(~(i, n), "/"), d), "("), p), ")") => {
-//      //println(p)
-//      def line2: Parser[List[(Candidate, Int, Int)]] = candidate ~ ";" ~ ";" ~ score ^^ {
-//        case ~(~(~(candidate, ";"), ";"),score) => {
-//          List((candidate, 0, score))
-//        }
-//      }
-//
-//      var temp: List[(Candidate, Int, Int)] = Nil
-//
-//      for (i <- p if !p.isEmpty) {
-//        parse(line2, i) match {
-//          case Success(sucLine, _) => temp = sucLine ::: temp
-//          case _ => throw new Exception("Should never happen")
-//        }
-//      }
-//
-//      temp = temp.sortWith(_._3 > _._3)
-//
-//      var ccand: List[Candidate] = Nil
-//
-//      for (f <- temp if !temp.isEmpty) {
-//        ccand = f._1 :: ccand
-//      }
-//
-//      WeightedBallot(ccand.reverse, i, Rational(n, d))
-//    }
-//  }
-//}
 
 object PreferencesWithoutIDAndWeightParser extends ElectionParser[WeightedBallot] with RegexParsers with ElectionParsers {
 
