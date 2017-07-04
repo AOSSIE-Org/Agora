5b4b31e9e8ede20eec43f69e63350dd3881157c0
SaumoPal97 <saumopal1997@gmail.com>
2017-07-05 02:08:33 +0530

    Modified candidate and candidate parser

--- src/main/scala/countvotes/parsers/CandidatesParser.scala
+++ src/main/scala/countvotes/parsers/CandidatesParser.scala
@@ -8,12 +8,14 @@ import scala.util.parsing.combinator._
 object CandidatesParser extends ElectionParser[Candidate] with RegexParsers {
 
   // the method line returns a Parser of type ACTBallotPapersDataStructure
-   def line : Parser[Candidate] = name ^^ {
-     case  n  =>
-       {  n
+   def line : Parser[Candidate] = name ~ opt(";") ~ opt(id) ~ opt(";") ~ opt(party) ^^ {
+     case  ~(~(~(~(name, _), id), _), party)  =>
+       {  Candidate(name, id, party)
        }
    }
 
-   def name : Parser[Candidate] = """[0-9A-Za-z\-\,\.\ \']*""".r ^^ { s => Candidate(s) }
+   def name : Parser[String] = """[0-9A-Za-z\-\,\.\ \']*""".r ^^ { _.toString }
+   def party : Parser[String] = """[0-9A-Za-z\-\,\.\ \']+""".r ^^ { _.toString }
+   def id : Parser[Int] = """[0-9]+""".r ^^ { _.toInt }
 
 }
--- src/main/scala/countvotes/structures/Candidate.scala
+++ src/main/scala/countvotes/structures/Candidate.scala
@@ -1,7 +1,28 @@
 package countvotes.structures
 
 case class Candidate(val name: String, val id: Option[Int] = None, val party: Option[String] = None) {
-  override def toString: String = name
+  id match {
+    case Some(i) => {
+      party match {
+        case Some(p) => {
+          def toString: String = "[" + i + "," + name + "," + p + "]"
+        }
+        case None => {
+          def toString: String = "[" + i + "," + name + "]"
+        }
+      }
+    }
+    case None => {
+      party match {
+        case Some(p) => {
+          def toString: String = "[" + name + "," + p + "]"
+        }
+        case None => {
+          def toString: String = name
+        }
+      }
+    }
+  }
 }
 
 //object Candidate {
