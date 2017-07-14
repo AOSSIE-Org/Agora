3b83755bea361ca5375b4b34dec78ca09094f8b9
Deepesh Pandey <deepeshpandey17@gmail.com>
2017-07-15 00:46:33 +0200

    updated

--- src/main/scala/countvotes/methods/VoteCountingMethod.scala
+++ src/main/scala/countvotes/methods/VoteCountingMethod.scala
@@ -50,15 +50,6 @@ abstract class VoteCountingMethod[B <: Ballot with Weight] {
    set.toList
   }
 
-  def getTotalVoters(election: Election[B]): Rational = {
-    var totalVoters = new Rational(0, 1)
-    for (b <- election) {
-
-      totalVoters = totalVoters + b.weight
-    }
-    totalVoters
-  }
-
  // just printing in terminal
  def printElection(election: Election[B]): Unit = {
     print("\n")
