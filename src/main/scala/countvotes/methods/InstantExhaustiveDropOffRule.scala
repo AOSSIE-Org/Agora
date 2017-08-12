d1dfdf2aa01325282a1271dce42f335f8abb83d0
SaumoPal97 <saumopal1997@gmail.com>
2017-08-13 01:08:23 +0530

    Fixed magic number issue and some case change for cleaner code

--- src/main/scala/countvotes/methods/InstantExhaustiveDropOffRule.scala
+++ src/main/scala/countvotes/methods/InstantExhaustiveDropOffRule.scala
@@ -16,9 +16,10 @@ object InstantExhaustiveDropOffRule extends VoteCountingMethod[WeightedBallot] {
   protected val report: Report[WeightedBallot] = new Report[WeightedBallot]
 
   var dropOffPercentage = Rational(0, 100)
+  val cutoffPercentage = 25 // drop off of candidates in steps of 5% till 25% after which lowest scoring candidate is eliminated
 
   def loser(candidate: (Candidate,Rational), total: Int, dropOffPercentage: Rational): Option[(Candidate,Rational)] = {
-    if (dropOffPercentage.numerator*(100/dropOffPercentage.denominator) > 25) {
+    if (dropOffPercentage.numerator*(100/dropOffPercentage.denominator) > cutoffPercentage) {
       //println(" 25% dropoff rule crossed, eliminating the last candidate " + candidate._1)
       Some(candidate)
     } else {
@@ -55,17 +56,18 @@ object InstantExhaustiveDropOffRule extends VoteCountingMethod[WeightedBallot] {
   override def winners(election: Election[WeightedBallot], ccandidates: List[Candidate], numVacancies: Int): List[(Candidate, Rational)] = {
 
     val majorityRational = Rational(1, 2)
+    val incrememtSize = 5 // drop off percentage increases by 5% in each round of elimination till 25%
     var tls = totals(election, ccandidates).toList.sortWith(_._2 > _._2)
     if (tls.size > 2) {
       if (tls.head._2 > majorityRational * election.size){
         tls.head :: List()
       } else {
-        dropOffPercentage = Rational(dropOffPercentage.numerator * (100 / dropOffPercentage.denominator) + 5, 100)
+        dropOffPercentage = Rational(dropOffPercentage.numerator * (100 / dropOffPercentage.denominator) + incrememtSize, 100)
         val losingCand: Option[(Candidate, Rational)] = loser(tls.last, election.size, dropOffPercentage)
         losingCand match {
-          case Some(c) => {
-            val newElection = exclude(election, c._1)
-            winners(newElection, ccandidates.filterNot(x => x == c._1), numVacancies)
+          case Some((c,_)) => {
+            val newElection = exclude(election, c)
+            winners(newElection, ccandidates.filterNot(x => x == c), numVacancies)
           }
           case None => {
             winners(election, ccandidates, numVacancies)
