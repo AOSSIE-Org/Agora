7a2ccf9acae4a611f07723e36a09fb7fc6f22ad5
SaumoPal97 <saumopal1997@gmail.com>
2017-08-15 14:30:09 +0530

    Removed redundant code

--- src/main/scala/countvotes/algorithms/Approval.scala
+++ src/main/scala/countvotes/algorithms/Approval.scala
@@ -9,7 +9,7 @@ import collection.mutable.{HashMap => MMap}
 trait SimpleApproval {
   def countApprovals(election: Election[WeightedBallot], candidates: List[Candidate]): MMap[Candidate, Rational] = {
     val m = new MMap[Candidate, Rational]
-    for (b <- election if !b.preferences.isEmpty) {
+    for (b <- election) {
       for(d <- b.preferences) {
         m(d) = b.weight + (m.getOrElse(d, 0))
       }
