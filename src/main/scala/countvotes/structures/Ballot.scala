bd4f36b517db1dfac5e6b289b099cbcefbd0e3d1
Bruno Woltzenlogel Paleo <bruno.wp@gmail.com>
2017-09-09 22:24:10 +1000

    Make ballot classes case classes

--- src/main/scala/countvotes/structures/Ballot.scala
+++ src/main/scala/countvotes/structures/Ballot.scala
@@ -26,7 +26,7 @@ object MarkedBallot{
   def apply(p: List[Candidate], id: Int, m:Boolean, w: Rational): MarkedBallot = new MarkedBallot(p, id, m, w)
 }
 
-class ScoreBallot(val scores: List[(Candidate, Rational)], id: Int, w: Rational) 
+case class ScoreBallot(scores: List[(Candidate, Rational)], override val id: Int, w: Rational) 
 extends Ballot(
   scores map { _._1}, 
   id, 
@@ -35,18 +35,10 @@ extends Ballot(
 {
   override def toString: String = "[" + id + ", " + scores + ", " + w + "]"
 }
-object ScoreBallot {
-  def apply(p: List[(Candidate, Rational)], id: Int, w: Rational): ScoreBallot = new ScoreBallot(p, id, w)
-}
 
-class RankBallot(val ranks: List[(Candidate, Int)], id: Int, w: Rational) extends Ballot(ranks map {
+case class RankBallot(val ranks: List[(Candidate, Int)], override val id: Int, w: Rational) extends Ballot(ranks map {
   _._1
 }, id, w) {
-
   override def toString: String = "[" + id + ", " + ranks + ", " + w + "]"
-}
-
-object RankBallot {
-  def apply(p: List[(Candidate, Int)], id: Int, w: Rational): RankBallot = new RankBallot(p, id, w)
 }
 
