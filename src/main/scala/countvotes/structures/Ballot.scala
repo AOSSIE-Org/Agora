f124b45a0179baf65781b6bfdc9833a04e276f8b
Bruno Woltzenlogel Paleo <bruno.wp@gmail.com>
2017-09-10 18:25:41 +1000

    Make `PreferenceBallot` a case class

--- src/main/scala/countvotes/structures/Ballot.scala
+++ src/main/scala/countvotes/structures/Ballot.scala
@@ -4,11 +4,8 @@ import scala.language.implicitConversions
 
 abstract class BallotBase(val id: Int, val weight: Rational)
 
-class PreferenceBallot(val preferences: List[Candidate], override val id: Int, override val weight: Rational)
+case class PreferenceBallot(val preferences: List[Candidate], override val id: Int, override val weight: Rational)
 extends BallotBase(id, weight)
-object PreferenceBallot {
-  def apply(p: List[Candidate], id: Int, w: Rational) = new PreferenceBallot(p, id, w)
-}
 
 case class ScoreBallot(scores: List[(Candidate, Rational)], override val id: Int, w: Rational) 
 extends BallotBase(id, w) 
