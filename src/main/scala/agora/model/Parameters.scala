7d130bdd6b0b685eee81fd580ee22a7c769d80a7
anga4 <anga4brandon500@gmail.com>
2019-06-21 12:43:14 +0100

    Implemented super majority method

--- src/main/scala/agora/model/Parameters.scala
+++ src/main/scala/agora/model/Parameters.scala
@@ -38,7 +38,9 @@ object ComparisonSets {
 
 case class Parameters(comparisonOrder: Option[Array[String]], allowedVote: Option[Int], cutOffQuota: Option[Double],
                       proportionalRatio: Option[Double], majorityBonus: Option[MajorityBonus],
-                      probabilityDistribution: Option[Array[Map[String, Double]]], comparisonSets: Option[ComparisonSets])
+                      probabilityDistribution: Option[Array[Map[String, Double]]], comparisonSets: Option[ComparisonSets],
+                      majorityPercentage: Option[Double]
+                     )
 
 object Parameters {
 
@@ -59,7 +61,8 @@ object Parameters {
       (__ \ "proportional_ratio").readNullable[Double] and
       (__ \ "majority_bonus").readNullable[MajorityBonus] and
       (__ \ "probability_distribution").readNullable[Array[Map[String, Double]]] and
-      (__ \ "comparison_sets").readNullable[ComparisonSets]
+      (__ \ "comparison_sets").readNullable[ComparisonSets] and
+      (__ \ "majorityPercentage").readNullable[Double]
     ) (Parameters.apply _)
 
 }
---
+++ src/main/scala/agora/votecounter/SuperMajority.scala
@@ -0,0 +1,25 @@
+package agora.votecounter
+
+import agora.model._
+import agora.model.{PreferenceBallot => Ballot}
+import spire.math.Rational
+
+object SuperMajority extends VoteCounter[Ballot] {
+
+  def runVoteCounter(e: Election[Ballot], candidates: List[Candidate], numVacancies: Int, param: Parameters):
+  List[(Candidate, Rational)] = {
+    val sortedList = e.firstVotes(candidates).toList sortWith {
+      (ct1, ct2) => ct1._2 > ct2._2
+    }
+    //Get majority percentage or default to 50%
+    val percentage = param.majorityPercentage.getOrElse(0.5)
+    if(percentage >= 0.5 && percentage <= 1.0) {
+      sortedList.take(numVacancies).filter { case (c, r) => r > (e.length * percentage) }
+    } else {
+      //Default to 50% if specified percentage is less than 50% or greater than 100%
+      sortedList.take(numVacancies).filter { case (c, r) => r > (e.length * 0.5) }
+    }
+  }
+
+  override def winners(e: Election[Ballot], ccandidates: List[Candidate], numVacancies: Int): List[(Candidate, Rational)] = ???
+}
